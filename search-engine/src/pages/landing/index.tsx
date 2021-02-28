65441229a0a4b2ad849a5211703b2125d76bb0d1
schemabuoi <charyszczakg@gmail.com>
2021-02-28 22:29:58 +0100

    Update landing page

--- search-engine/src/pages/landing/index.tsx
+++ search-engine/src/pages/landing/index.tsx
@@ -6,6 +6,7 @@ import { ResponseStatus } from "../../services/response/IResponse";
 import { AppLogger } from "../../utilities/app-logger/AppLogger";
 import { LogRecipient } from "../../utilities/app-logger/models/LogRecipient";
 import { LogType } from "../../utilities/app-logger/models/LogType";
+import "line-awesome/dist/line-awesome/css/line-awesome.min.css";
 import css from "./index.module.scss";
 
 export default function Landing() {
@@ -44,19 +45,44 @@ export default function Landing() {
     <div className={css.mainContainer}>
       {documents ? (
         <div className={css.mainWrapper}>
+          <div className={css.headerContainer}>
+          <div className={css.headerWrapper}>
+            <div className={css.searchContainer}>
+              <input className={css.searchInput} placeholder="Search documents">
+              </input>
+              <div className={css.searchIcon}>
+              <i className="las la-search"></i>
+              </div>
+            </div>
+          </div>
+          </div> 
           <div className={css.documentsContainer}>
+            <div className={css.documentsWrapper}>
             {documents.map(document => {
               return (
                 <div className={css.document} key={document.id}>
-                  {document.id}
+                  <div className={css.fileIcon}>
+                    <i className="las la-file-alt"></i>
+                  </div>
+                  <div className={css.documentDataContainer}>
+                    
+                    <div className={css.documentNameContainer}>
+                      {document.name}
+                    </div>
+                    <div className={css.documentIdContainer}>
+                      ID: {document.id}
+                    </div>
+                  </div>
                 </div>
               )
             })}
+            </div>
+            
           </div>
         </div>
       ) : (
           <div className={css.loadingContainer}>
-            <ThreeDots color={"#FFAB73"} />
+            <ThreeDots color={"#FF7b54"} />
           </div>
         )}
     </div>
